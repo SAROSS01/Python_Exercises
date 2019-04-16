06a9f7d22e98faba6a554799641148315e828909
SAROSS01 <stevenar2016@gmail.com>
2019-04-15 21:12:26 -0400

    "Initial commit"

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Steven A Ross'
+my_age = 22
+my_weight = 160 * 0.45359237
+my_height = 69 * 2.54
+my_eyes = "Brown"
+my_skin = "BLACK"
+my_hair = "black"
+my_teeth = "white"
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm tall.")
+print(f"He's {round (my_weight)} kg heavy.")
+print("He's a skinny nigga with a big dick")
+print(f"He's got {my_eyes} eyes, he's got {my_skin} skin, and {my_hair} hair.")
+print(f"His teeth are {my_teeth} unless he's drinking acid.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_weight}, and {my_height} I get {total}.")
